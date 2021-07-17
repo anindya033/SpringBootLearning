2ecc643100a116caa33cae9b027fb807350758c8
anindya033 <anindya033paul@gmail.com>
2021-07-17 10:35:19 +0530

    # Logger added in startup class

--- src/main/java/com/dream/wave/BuildYourHomeApplication.java
+++ src/main/java/com/dream/wave/BuildYourHomeApplication.java
@@ -1,13 +1,25 @@
 package com.dream.wave;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 @SpringBootApplication
-public class BuildYourHomeApplication {
+public class BuildYourHomeApplication implements ApplicationRunner {
 
+	static final Logger logger = LogManager.getLogger(BuildYourHomeApplication.class.getName());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BuildYourHomeApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("Logging start");
 	}
 
 }
